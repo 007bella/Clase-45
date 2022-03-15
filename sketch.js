00b228467ba5942eff8c8d9c8bf4724b937770e4
007bella <95721329+007bella@users.noreply.github.com>
2022-03-15 16:05:12 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,84 +1,156 @@
-var apple,orangeL,redL;
-var carrotImg,orangeImg,redImg;
-var cocina, cocinaImg;
-var chile, chileImg;
-var lechuga, lechugaImg;
-var carne, carneImg;
-var hamburguesa, hamburguesaImg;
-var mostaza, mostazaImg;
-var jitomate, jitomateImg;
-var ketchup, ketchupImg;
-var chef, chefImg;
-var moneda, monedaImg;
-var ingredientes;
 
-function preload(){
-  appleImg = loadImage("apple.png");
-  orangeImg = loadImage("orangeLeaf.png");
-  redImg = loadImage("redImage.png");
-  monedaImg = loadImage("moneda.png");
-  chefImg = loadImage("chef.png");
-  cocinaImg = loadImage("Cocina.jpg");
-  ketchupImg = loadImage("kepchup.png");
-  jitomateImg = loadImage("Jitomate.png");
-  chileImg = loadImage("chile.png");
-  mostazaImg = loadImage("Mostaza.png");
-  hamburguesaImg = loadImage("hamburgesa.png");
-  carneImg = loadImage("carne.png");
-  lechuga = loadImage("lechuga.png");
+  var score = 1;
+  var gameOver, gameOverImg
+ var appleImg;
+ var carrotImg,orangeImg,redImg;
+ var cocina, cocinaImg;
+ var chile, chileImg;
+ var lechuga, lechugaImg;
+ var carne, carneImg;
+ var hamburguesa, hamburguesaImg;
+ var mostaza, mostazaImg;
+ var jitomate, jitomateImg;
+ var ketchup, ketchupImg;
+ var chef, chefImg;
+ var moneda, monedaImg;
+ var ingredientes;
+  var obstacleGroup;
+  var restart;
 
-}
+ function preload(){
+   appleImg = loadImage("apple.png");
+   orangeImg = loadImage("orangeLeaf.png");
+   redImg = loadImage("redImage.png");
+   monedaImg = loadImage("moneda.png");
+   chefImg = loadImage("chef.png");
+   cocinaImg = loadImage("Cocina.jpg");
+   ketchupImg = loadImage("kepchup.png");
+   jitomateImg = loadImage("Jitomate.png");
+   chileImg = loadImage("chile.png");
+   mostazaImg = loadImage("Mostaza.png");
+   hamburguesaImg = loadImage("hamburgesa.png");
+   carneImg = loadImage("carne.png");
+   lechuga = loadImage("lechuga.png");
+   gameOverImg= loadImage("assets/gameOver.png");
+   restartImg = loadImage("assets/restart.png");
+ }
 
-
-function setup(){
+ function setup(){
   
-  createCanvas(800,450);
-// Fondo en movimiento
-cocina=createSprite(400,225,0,0);
-cocina.addImage(cocinaImg);
-cocina.scale = 1.4;
+   createCanvas(800,450);
+ cocina=createSprite(400,225,0,0);
+ cocina.addImage(cocinaImg);
+ cocina.scale = 1.4;
 
- chef = createSprite(160,400,100,60);
- chef.scale = 1.5;
- chef.addImage(chefImg);
+  chef = createSprite(160,400,100,60);
+  chef.scale = 1.5;
+  chef.addImage(chefImg);
 
- moneda = createSprite(50,50,50,50);
- moneda.scale = 0.09;
- moneda.addImage(monedaImg);
+  moneda = createSprite(50,50,50,50);
+  moneda.scale = 0.09;
+  moneda.addImage(monedaImg);
 
-}
+  obstacleGroup = new Group();
+  ingredientes = new Group();
+
+  gameOver = createSprite(220,200);
+  restart = createSprite(220,240);
+  gameOver.addImage(gameOverImg);
+  gameOver.scale = 0.5;
+  restart.addImage(restartImg);
+  restart.scale = 0.5;
+  gameOver.visible = false;
+  restart.visible = false;
+
+ }
 
 function draw() {
   background(0);
-  
-  // Niño moviéndose en el eje X con el mouse
-  chef.x = World.mouseX;
-  
-  edges= createEdgeSprites();
-  chef.collide(edges);
-  spawnIngredientes();
-   drawSprites();
- /*   var select_sprites = Math.round(random(1,12));
-  
-   if (frameCount % 80 == 0) {
-     if (select_sprites ) {
-        createRed();
-     } else if (select_sprites) {
-       createOrange();
-     }
-   }  */
- 
+   
+   if(gameState === PLAY){
+    chef.x = World.mouseX;
+   
+   }     
+
+   if(gameState === END) 
+    {
+      score = 0
+      gameOver.visible = true;
+      gameOver.depth = gameOver.depth+1
+      restart.visible = true;
+      restart.depth = restart.depth+1
+
+          chef.velocityX = 0;
+          chef.velocityY = 0;
+          obstacleGroup.setVelocityXEach(0);
+          ingredientes.setVelocityXEach(0);
+        
+          obstacleGroup.setLifetimeEach(-1);
+          ingredientes.setLifetimeEach(-1);
+         
+          chef.y = 200;
+
+          if(mousePressedOver(restart)) 
+          {
+                reset();
+          }
+
+    } 
+    spawnObstaclesTop();
+    ingredientesTop();
+      drawSprites();
+      Score();  
 }
-function spawnIngredientes() 
+
+function reset()
+{
+  gameState = PLAY;
+  gameOver.visible = false;
+  restart.visible = false;
+  obstacleGroup.destroyEach();
+  ingredientes.destroyEach();
+
+  score = 1;
+}
+
+
+function spawnObstaclesTop() 
 {
       if(World.frameCount % 60 === 0) {
-        ingredientes = createSprite(400,40,40,50);
+        obstacleGroup = createSprite(400,50,40,50);
+    
+    obstacleGroup.scale = 0.1;
+    obstacleGroup.velocityX = -4;
+
+
+    obstacleGroup.y = Math.round(random(10,100));
+
+    var rand = Math.round(random(1,2));
+    switch(rand) {
+      case 1: obstacleGroup.addImage(orangeImg);
+              break;
+      case 2: obstacleGroup.addImage(redImg);
+              break;
+      default: break;
+    }
+
+   obstacleGroup.lifetime = 100;
+    
+   
+      }
+}
+
+function ingredientesTop() 
+{
+      if(World.frameCount % 60 === 0) {
+        obstacleGroup = createSprite(400,50,40,50);
     
     ingredientes.scale = 0.1;
-    ingredientes.velocityY = 3;
+    ingredientes.velocityX = -4;
 
-    ingredientes.x = Math.round(random(10,400));
- 
+
+    ingredientes.y = Math.round(random(10,100));
+
     var rand = Math.round(random(1,7));
     switch(rand) {
       case 1: ingredientes.addImage(chile);
@@ -95,74 +167,32 @@ function spawnIngredientes()
               break;
       case 7: ingredientes.addImage(apple);
               break;
+
       default: break;
-      
     }
-    ingredientes.lifetime = 150;
-    chef.depth = chef.depth + 1;
-  }
+
+   ingredientes.lifetime = 100;
+    
+   
+      }
 }
 
-// function createChile(){
-// chile = createSprite (40, 10, 10, 10);
-// chile.addImage(chileImg);
-// chile.scale=0.07;
-// chile.velocityY = 3;
 
-// }
-// function createCarne(){
-// carne = createSprite(40,10, 10, 10);
-// carne.addImage(carneImg);
-// carne.scale=0.07;
-// carne.velocityY = 3;
-// }
+function Score()
+{
+         if(chef.isTouching(ingredientes))
+         {
+           score = score + 1;
+         }
+         else(chef.isTouching(obstacleGroup))
+         {
+           score = score - 1;
+         }
 
-// function createHamburguesa(){
-// hamburguesa = createSprite(40,10, 10, 10);
-// hamburguesa.addImage(hamburguesaImg);
-// hamburguesa.scale=0.07;
-// hamburguesa.velocityY = 3;
-// }
+        textFont("algerian");
+        textSize(30);
+        fill("Black");
+        text("Puntuación: "+ score, 100, 50);
+}
+ 
 
-// function createJitomate(){
-// jitomate = createSprite(40,10, 10, 10);
-// jitomate.addImage(jitomateImg);
-// jitomate.scale=0.07;
-// jitomate.velocityY = 3;
-// }
-
-// function createKepchup(){
-// ketchup = createSprite(40,10, 10, 10);
-// ketchup.addImage(ketchupImg);
-// ketchup.scale=0.07;
-// ketchup.velocityY = 3;
-// }
-
-// function createLechuga(){
-// lechuga = createSprite(40,10, 10, 10);
-// lechuga.addImage(lechugaImg);
-// lechuga.scale=0.07;
-// lechuga.velocityY = 3;
-// }
-
-// function createApples() {
-// apple = createSprite(40,10, 10, 10);
-// apple.addImage(appleImg);
-// apple.scale=0.07;
-// apple.velocityY = 3;
-  
-// }
-
-function createOrange() {
- orangeL = createSprite(40,10, 10, 10);
- orangeL.addImage(orangeImg);
- orangeL.scale=0.08;
- orangeL.velocityY = 3;
- }
-
- function createRed() {
- redL = createSprite(40,10, 10, 10);
- redL.addImage(redImg);
- redL.scale=0.06;
-   redL.velocityY = 3;
- }
