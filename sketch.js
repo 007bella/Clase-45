c7a422b6b8df6f16e6f223a4f965a6a071a1d163
007bella <95721329+007bella@users.noreply.github.com>
2022-03-14 18:02:03 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ var jitomate, jitomateImg;
 var ketchup, ketchupImg;
 var chef, chefImg;
 var moneda, monedaImg;
+var ingredientes;
 
 function preload(){
   appleImg = loadImage("apple.png");
@@ -44,6 +45,7 @@ cocina.scale = 1.4;
  moneda = createSprite(50,50,50,50);
  moneda.scale = 0.09;
  moneda.addImage(monedaImg);
+
 }
 
 function draw() {
@@ -54,40 +56,28 @@ function draw() {
   
   edges= createEdgeSprites();
   chef.collide(edges);
-  
+  spawnIngredientes();
    drawSprites();
+ /*   var select_sprites = Math.round(random(1,12));
   
-  // var select_sprites = Math.round(random(1,12));
-  
-  // if (frameCount % 80 == 0) {
-  //   if (select_sprites ) {
-  //     createApples();
-  //   } else if (select_sprites) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
-  //  if(frameCount % 80 == 0) {
-  //    if (select_sprites ) {
-  //      createChile();
-  //    }else if(select_sprites ){
-  //      createCarne();
-  //    }else{
-  //      createHamburguesa();
-  //    }
-  //  }
+   if (frameCount % 80 == 0) {
+     if (select_sprites ) {
+        createRed();
+     } else if (select_sprites) {
+       createOrange();
+     }
+   }  */
+ 
 }
 function spawnIngredientes() 
 {
-      if(World.frameCount % 80 === 0) {
-        ingredientes = createSprite(700,400,40,50);
+      if(World.frameCount % 60 === 0) {
+        ingredientes = createSprite(400,40,40,50);
     
     ingredientes.scale = 0.1;
     ingredientes.velocityY = 3;
 
-
-    ingredientes.x = Math.round(random(10,100));
+    ingredientes.x = Math.round(random(10,400));
  
     var rand = Math.round(random(1,7));
     switch(rand) {
@@ -97,11 +87,11 @@ function spawnIngredientes()
               break;
       case 3: ingredientes.addImage(carne);
               break;
-      case 4: ingredientes.addImage(jitomate);
+      case 4: ingredientes.addImage(lechuga);
               break;
       case 5: ingredientes.addImage(ketchup);
               break;
-      case 6: ingredientes.addImage(lechuga);
+      case 6: ingredientes.addImage(jitomate);
               break;
       case 7: ingredientes.addImage(apple);
               break;
@@ -109,68 +99,70 @@ function spawnIngredientes()
       
     }
     ingredientes.lifetime = 150;
+    chef.depth = chef.depth + 1;
   }
 }
-function createChile(){
-  chile = createSprite (40, 10, 10, 10);
-chile.addImage(chileImg);
-chile.scale=0.07;
-chile.velocityY = 3;
 
-}
-function createCarne(){
-carne = createSprite(40,10, 10, 10);
-carne.addImage(carneImg);
-carne.scale=0.07;
-carne.velocityY = 3;
-}
+// function createChile(){
+// chile = createSprite (40, 10, 10, 10);
+// chile.addImage(chileImg);
+// chile.scale=0.07;
+// chile.velocityY = 3;
 
-function createHamburguesa(){
-hamburguesa = createSprite(40,10, 10, 10);
-hamburguesa.addImage(hamburguesaImg);
-hamburguesa.scale=0.07;
-hamburguesa.velocityY = 3;
-}
+// }
+// function createCarne(){
+// carne = createSprite(40,10, 10, 10);
+// carne.addImage(carneImg);
+// carne.scale=0.07;
+// carne.velocityY = 3;
+// }
 
-function createJitomate(){
-jitomate = createSprite(40,10, 10, 10);
-jitomate.addImage(jitomateImg);
-jitomate.scale=0.07;
-jitomate.velocityY = 3;
-}
+// function createHamburguesa(){
+// hamburguesa = createSprite(40,10, 10, 10);
+// hamburguesa.addImage(hamburguesaImg);
+// hamburguesa.scale=0.07;
+// hamburguesa.velocityY = 3;
+// }
 
-function createKepchup(){
-ketchup = createSprite(40,10, 10, 10);
-ketchup.addImage(ketchupImg);
-ketchup.scale=0.07;
-ketchup.velocityY = 3;
-}
+// function createJitomate(){
+// jitomate = createSprite(40,10, 10, 10);
+// jitomate.addImage(jitomateImg);
+// jitomate.scale=0.07;
+// jitomate.velocityY = 3;
+// }
 
-function createLechuga(){
-lechuga = createSprite(40,10, 10, 10);
-lechuga.addImage(lechugaImg);
-lechuga.scale=0.07;
-lechuga.velocityY = 3;
-}
+// function createKepchup(){
+// ketchup = createSprite(40,10, 10, 10);
+// ketchup.addImage(ketchupImg);
+// ketchup.scale=0.07;
+// ketchup.velocityY = 3;
+// }
 
-function createApples() {
-apple = createSprite(40,10, 10, 10);
-apple.addImage(appleImg);
-apple.scale=0.07;
-apple.velocityY = 3;
+// function createLechuga(){
+// lechuga = createSprite(40,10, 10, 10);
+// lechuga.addImage(lechugaImg);
+// lechuga.scale=0.07;
+// lechuga.velocityY = 3;
+// }
+
+// function createApples() {
+// apple = createSprite(40,10, 10, 10);
+// apple.addImage(appleImg);
+// apple.scale=0.07;
+// apple.velocityY = 3;
   
-}
+// }
 
 function createOrange() {
-orangeL = createSprite(40,10, 10, 10);
-orangeL.addImage(orangeImg);
-orangeL.scale=0.08;
-orangeL.velocityY = 3;
-}
+ orangeL = createSprite(40,10, 10, 10);
+ orangeL.addImage(orangeImg);
+ orangeL.scale=0.08;
+ orangeL.velocityY = 3;
+ }
 
-function createRed() {
-redL = createSprite(40,10, 10, 10);
-redL.addImage(redImg);
-redL.scale=0.06;
-  redL.velocityY = 3;
-}
+ function createRed() {
+ redL = createSprite(40,10, 10, 10);
+ redL.addImage(redImg);
+ redL.scale=0.06;
+   redL.velocityY = 3;
+ }
